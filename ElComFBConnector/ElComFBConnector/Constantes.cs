29e36c28e0a57980faf0486f385b33b716ed50f8
Oscar Ramos <oscarramosp@gmail.com>
2015-03-21 15:45:30 -0500

    testt

--- ElComFBConnector/ElComFBConnector/Constantes.cs
+++ ElComFBConnector/ElComFBConnector/Constantes.cs
@@ -10,6 +10,10 @@ namespace ElComFBConnector
         public const string AppID = "884427038267146";
         public const string AppSecret = "0a60d73f092c689208699724f8b0933d";
 
+        public const string test = "/v2.2/{user-id}/likes/{page-id}";
+
+        public const string comercioFBId = "71263708835";
+
         public const string ApiBaseUrl = "https://graph.facebook.com/oauth/access_token";
     }
 }
